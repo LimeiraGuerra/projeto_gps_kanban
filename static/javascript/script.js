acb40d4ab0d36419c7174e491c44d15b7fcc668e
Raiane-nepomuceno <nepomuceno.raiane@aluno.ifsp.edu.br>
2020-11-11 11:06:33 -0300

    tabela

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,15 +1,13 @@
-function capturar()
-{
-    var nome = document.getElementById('name-advogado').value;
-    var oab = documet.getElementById('oab').value;
-    var table = document.getElementeByTagName('table')[0];
-
-    var newRow = table.insertRow(1);
-    document.getElementById('valor').innerHTML = nome; 
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-
-    cell1.innerHTML = oab;
-    cell2.innerHTML = nome;
-
-}
+	function capturar()
+	{
+        var nome = document.getElementById('name-advogado').value;
+        var oab = document.getElementById('oab').value;
+        var table = document.getElementById('Tabela');
+        var newRow = table.insertRow(0);
+       
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+    
+        cell1.innerHTML = oab;
+        cell2.innerHTML = nome;
+     }
