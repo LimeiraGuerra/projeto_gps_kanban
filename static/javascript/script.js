884b8d480ff30ed5b6e76799d2333cb591b809e7
Raiane-nepomuceno <nepomuceno.raiane@aluno.ifsp.edu.br>
2020-11-11 13:52:32 -0300

    controlando a inserção na tabela

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -2,12 +2,18 @@
 	{
         var nome = document.getElementById('name-advogado').value;
         var oab = document.getElementById('oab').value;
-        var table = document.getElementById('Tabela');
-        var newRow = table.insertRow(0);
-       
-        var cell1 = newRow.insertCell(0);
-        var cell2 = newRow.insertCell(1);
-    
-        cell1.innerHTML = oab;
-        cell2.innerHTML = nome;
-     }
+        if(nome == null || oab == null)
+        {
+           alert("Campos nulos. Por favor, digite novamente!")
+        }
+        else{
+                var table = document.getElementById('Tabela');
+                var newRow = table.insertRow(0);
+               
+                var cell1 = newRow.insertCell(0);
+                var cell2 = newRow.insertCell(1);
+            
+                cell1.innerHTML = oab;
+                cell2.innerHTML = nome;        
+        }
+         }
