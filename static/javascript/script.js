5f556b84f7d923fdee0a59ee53f0d68d2e5a996a
Raiane-nepomuceno <nepomuceno.raiane@aluno.ifsp.edu.br>
2020-11-11 09:51:50 -0300

    iniciando js

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -4,8 +4,7 @@ function capturar()
 {
     nome = document.getElementById('name-advogado').value;
     oab = documet.getElementById('oab').value;
-    alert(aob);
-    document.write("<p>"+aob+"</p>")
+    //document.write("<p>"+aob+"</p>")
     //document.write("<p>"+nome+"</p>");
     document.getElementById('valor').innerHTML = nome; 
 }
