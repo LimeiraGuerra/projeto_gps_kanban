227d29dfea9357a8ebfe393ad1036f93a927a3cf
Raiane-nepomuceno <nepomuceno.raiane@aluno.ifsp.edu.br>
2020-11-13 21:04:19 -0300

    botao excluir

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -6,15 +6,19 @@
            alert("Por favor, digite novamente os dados do advogado!")
         }
         else{
-                var nome = document.getElementById('name-advogado').value;
+                var name = document.getElementById('name-advogado').value;
                 var oab = document.getElementById('oab').value;
         
                 var table = document.getElementById('Tabela');
+
                 var newRow = table.insertRow(0);
                
                 var cell1 = newRow.insertCell(0);
                 var cell2 = newRow.insertCell(1);
+                var cell3 = newRow.insertCell(2);
+                
                 cell1.innerHTML = oab;
-                cell2.innerHTML = nome;        
+                cell2.innerHTML = name;        
+                cell3.innerHTML = '<input type="button" class="btn btn-danger" value="X" onclick="deleteRow(this)"/>';
         }
          }
