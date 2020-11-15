509e684ca2d532ea5ecc7ba526fdb4994760ec7b
Raiane-nepomuceno <nepomuceno.raiane@aluno.ifsp.edu.br>
2020-11-15 18:30:48 -0300

    detalhes

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -17,9 +17,13 @@
                 var cell2 = newRow.insertCell(1);
                 var cell3 = newRow.insertCell(2);
                 
+                cell1.className = 'coluna-nome-advogado';
+                cell2.className = 'coluna-num-oab';
+                cell3.className = 'coluna-btn';
+
                 cell1.innerHTML = oab;
                 cell2.innerHTML = name;        
-                cell3.innerHTML = '<input type="button" class="btn btn-danger" value="X" onclick="deleteRow(this)"/>';
+                cell3.innerHTML = '<input type="button" class="btn btn-danger" id = "btn-remove" value="X" onclick="deleteRow(this)"/>';
         }
         
          }
