82bf105d8591f4fb87b10b48a53762c3c251b0b4
Erika Raissa Bueno da Silva <erikaaraissaqwe@gmail.com>
2020-11-26 13:46:45 -0300

    algumas verificações

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,19 +1,20 @@
-function capturar() {
-	if (document.getElementById('name-advogado').value == '' || document.getElementById('oab').value == '') {
 
-		alert("Por favor, digite novamente os dados do advogado!")
+function capturar() {
+	let name = document.getElementById('name-advogado').value.trim();
+	let oab = document.getElementById('oab').value.trim();
+
+	if (isEmpty(name) || isEmpty(oab) ) {
+		alert("Por favor, digite novamente os dados do advogado!");
 	}
 	else {
-		var name = document.getElementById('name-advogado').value;
-		var oab = document.getElementById('oab').value;
+		
+		let table = document.getElementById('Tabela');
 
-		var table = document.getElementById('Tabela');
+		let newRow = table.insertRow(0);
 
-		var newRow = table.insertRow(0);
-
-		var cell1 = newRow.insertCell(0);
-		var cell2 = newRow.insertCell(1);
-		var cell3 = newRow.insertCell(2);
+		let cell1 = newRow.insertCell(0);
+		let cell2 = newRow.insertCell(1);
+		let cell3 = newRow.insertCell(2);
 
 		cell1.className = 'coluna-nome-advogado';
 		cell2.className = 'coluna-num-oab';
@@ -22,12 +23,55 @@ function capturar() {
 		cell1.innerHTML = oab;
 		cell2.innerHTML = name;
 		cell3.innerHTML = '<input type="button" class="btn btn-danger" id = "btn-remove" value="X" onclick="deleteRow(this)"/>';
+		clearInput('oab');
+		clearInput('name-advogado')
 	}
 
 }
 function deleteRow(t) {
-	var row = t.parentNode.parentNode;
+	let row = t.parentNode.parentNode;
 	document.getElementById("Tabela").deleteRow(row.rowIndex);
-	console.log(row);
+}
 
+function clearTable(){
+	let table = document.getElementById("Tabela");
+	let linhas = table.rows;
+	for (let i = linhas.length-1; i >= 0; i = i-1){
+		table.deleteRow(i);
+	}
+}
+
+function cancel(){
+	clearTable();
+	clearAllInputs();
+}
+
+function clearInput(idInput){
+	document.getElementById(idInput).value = '';
+}
+
+function isEmpty(str) {
+    return (str.length === 0);
+}
+
+function addTask(){
+	let nameTask = document.getElementById('name-task').value.trim();
+	let descricao = document.getElementById('desc').value.trim();
+
+	if(isEmpty(nameTask) || isEmpty(descricao)){
+		alert("Por favor, digite todos os dados da tarefa!");
+	}
+	else{
+		alert("vai ser add");
+		clearTable();
+		clearAllInputs();
+	}
+	
+}
+
+function clearAllInputs(){
+	clearInput('oab');
+	clearInput('name-task');
+	clearInput('name-advogado');
+	clearInput('desc');
 }
